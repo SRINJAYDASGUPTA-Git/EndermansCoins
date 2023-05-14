0d64716c563bd9218d91bd7407bb2a9a68a17031
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2023-05-14 23:26:13 +0530

    init

---
+++ src/main/java/net/endermans/coins/EndermansCoinsClient.java
@@ -0,0 +1,10 @@
+package net.endermans.coins;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EndermansCoinsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
