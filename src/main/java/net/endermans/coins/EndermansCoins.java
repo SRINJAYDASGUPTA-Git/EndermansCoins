331a9297d288cdbb9ace05522d017d32ce50c9c3
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2023-05-14 23:21:36 +0530

    Initial Commit

---
+++ src/main/java/net/endermans/coins/EndermansCoins.java
@@ -0,0 +1,16 @@
+package net.endermans.coins;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EndermansCoins implements ModInitializer {
+   public static final String MOD_ID = "endermans";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hello Fabric world!");
+    }
+}
